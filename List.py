c848b079d86249e7a778e15984be2f32abcf8474
Eduard Humberto Buitrago Jaime <eduard.buitrago@javeriana.edu.co>
2022-08-30 12:27:11 -0500

    agregamos el color rosado

--- List.py
+++ List.py
@@ -1,6 +1,6 @@
 #Lista de ejemplo
-colors = ['red', 'green', 'yellow']
+colors = ['red', 'green', 'yellow', 'blue']
 
 # Estructura de listas
-colors.insert(len(colors),'Azul')
+colors.insert(len(colors), 'rosado')
 print(colors)
