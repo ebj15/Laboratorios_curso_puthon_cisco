21f9a3b17e45ae4a3b6b148686c6344130f0019d
ebj15 <77462723+ebj15@users.noreply.github.com>
2022-11-17 20:30:24 -0500

    Tablero ok

--- tic_tac/Triqui2.py
+++ tic_tac/Triqui2.py
@@ -1,1 +1,18 @@
-print( ("+" + "-"*7)*3,end="+")
+line1 = "+-------+-------+-------+"
+line2 = "|       |       |       |"
+Lista1= ["|  ",1,"  |"," ",2,"  |","  ",3," |"]
+Lista2= ["|  ",5,"  |"," ",6,"  |","  ",7," |"]
+Lista3= ["|  ",8,"  |"," ",9,"  |","  ",0," |"]
+print(line1)
+print(line2)
+print(*Lista1)
+print(line2)
+print(line1)
+print(line2)
+print(*Lista2)
+print(line2)
+print(line1)
+print(line2)
+print(*Lista3)
+print(line2)
+print(line1)
